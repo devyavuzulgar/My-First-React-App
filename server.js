c6988a6d2324a00508195d37727861c88304f96c
Yavuz <digitalworksyavuz@gmail.com>
2024-09-11 10:44:04 +0300

    project learn react

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+import "express-async-errors";
 import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 const app = express();
 import morgan from "morgan";
 import { nanoid } from "nanoid";
+import jobRouter from "./routers/jobRouter";
+import mongoose from "mongoose";
 
 let jobs = [
   { id: nanoid(), company: "apple", position: "front-end" },
@@ -24,13 +27,32 @@ app.get("/api/v1/jobs", (req, res) => {
   res.status(200).json({ jobs });
 });
 
+app.use("/api/v1/jobs", jobRouter);
+
 app.post("/", (req, res) => {
   console.log(req);
 
   res.json({ message: "Data received", data: req.body });
 });
 
-const port = process.env.PORT || 5100;
-app.listen(port, () => {
-  console.log(`server running on PORT ${port}....`);
+// Not Found Middleware
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
 });
+
+// Error Middleware
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ msg: "something went wrong" });
+});
+
+const port = process.env.PORT || 5100;
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on PORT ${port}....`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
