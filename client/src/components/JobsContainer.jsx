0aa121b27c1aee74dee400df28861a123b32f8a1
Yavuz <digitalworksyavuz@gmail.com>
2024-10-01 14:30:53 +0300

    learn react

--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Job from "./Job.jsx";
+import Job from "./Job";
 import Wrapper from "../assets/wrappers/JobsContainer";
-
-import { useAllJobsContext } from "../pages/AllJobs.jsx";
+import PageBtnContainer from "./PageBtnContainer";
+import { useAllJobsContext } from "../pages/AllJobs";
 
 const JobsContainer = () => {
   const { data } = useAllJobsContext();
-  const { jobs } = data;
+  const { jobs, totalJobs, numOfPages } = data;
   if (jobs.length === 0) {
     return (
       <Wrapper>
@@ -17,13 +17,15 @@ const JobsContainer = () => {
 
   return (
     <Wrapper>
+      <h5>
+        {totalJobs} job{jobs.length > 1 && "s"} found
+      </h5>
       <div className="jobs">
         {jobs.map((job) => {
           return <Job key={job._id} {...job} />;
         })}
       </div>
+      {numOfPages > 1 && <PageBtnContainer />}
     </Wrapper>
   );
 };
-
-export default JobsContainer;
